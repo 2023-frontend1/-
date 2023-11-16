1f79745ff2dcaee1c643eece34dcb556d82309ac
yunshin <2younsin@gmail.com>
2023-11-17 02:35:25 +0900

    ✨ Feat: Design Token export

---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import Color from './Color'
+import DisplayFlex from './DisplayFlex'
+import FontSize from './FontSize'
+import FontWeight from './FontWeight'
+export { Color, DisplayFlex, FontSize, FontWeight }
