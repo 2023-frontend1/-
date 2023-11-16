88e92ec591a56e5e3bdf639aa18c879857a720bf
snkchan <qkrcodbs122@gmail.com>
2023-11-16 18:48:28 +0900

    ✨ Feat: ImageBox컴포넌트 생성 및 props 설정

--- src/components/imageBox/ImageBox.jsx
+++ src/components/imageBox/ImageBox.jsx
@@ -1,5 +1,7 @@
-const ImageBox = () => {
-	return <div>ImageBox</div>
+import ImgBox from "./ImageBox.styles"
+
+const ImageBox = ({ boxshape, size, src, ...rest }) => {
+  return <ImgBox $boxshape={boxshape} $size={size} src={src} {...rest} />
 }
 
 export default ImageBox
