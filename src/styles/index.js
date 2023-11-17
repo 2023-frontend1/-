1481a66f379f3d0eb3449113b444217cfaf63893
yunshin <2younsin@gmail.com>
2023-11-17 09:33:01 +0900

    🚚 Move: Color, FontSize, FontWeight 객체의 폴더 변경

--- src/styles/index.js
+++
@@ -1,5 +0,0 @@
-import Color from './Color'
-import DisplayFlex from './DisplayFlex'
-import FontSize from './FontSize'
-import FontWeight from './FontWeight'
-export { Color, DisplayFlex, FontSize, FontWeight }
