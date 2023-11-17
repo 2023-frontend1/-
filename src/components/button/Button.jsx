2a7ce2d661e2b608a3b7131365f38ce1abbf936e
hyetjs <cherryade98@gmail.com>
2023-11-17 16:33:16 +0900

    ✨ Feat: Button 컴포넌트 생성 및 props 설정

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import ButtonStyle from './Button.styles'
 
 const Button = ({ variant, size, shape, children, ...rest }) => {
 	return (
-		<ButtonStyle {...{ variant, size, shape }} {...rest}>
+		<ButtonStyle {...{ variant, size, shape, ...{ rest } }}>
 			{children}
 		</ButtonStyle>
 	)
