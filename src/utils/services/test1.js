f45baf7a71ad24379bc74a683dc218a42bf9df4e
yunshin <2younsin@gmail.com>
2023-11-21 21:48:41 +0900

    🔥 Remove: 테스트 파일 삭제

--- src/utils/services/test1.js
+++
@@ -1,4 +0,0 @@
-const Test1Func = (number) => {
-	console.log(number)
-}
-export { Test1Func }
--- src/utils/services/test2.js
+++
@@ -1,3 +0,0 @@
-import UserService from './UserService.js'
-
-console.log(UserService.GetUsersInfoByUserId(1))
